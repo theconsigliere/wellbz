91e4ae17f2353c6d2542111073c12e01dad99dc5
max-work-mac <max@dirty-martini.com>
2019-04-03 10:38:23 +0100

    pn video 13

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,19 @@
 // Put your applicaiton javascript here
+
+window.onload = function() {
+  var jsButton = document.querySelectorAll(".js-quantity-button");
+  var form = document.getElementById("AddToCartForm");
+  var quantity = form.querySelector("js-quantity-field");
+  var quantityValue = parseInt(quantity.val());
+  var max = quantity.attr("max") ? parseInt(quantity.attr("max")) : null;
+
+  jsButton.forEach(button => {
+    button.addEventListener("click", () => {
+      // alert("button clicked");
+      if (jsButton.classList.contains(".plus")) {
+        quantity.val(quantityValue + 1);
+      } else if (jsButton.classList.contains(".minus")) {
+      }
+    });
+  });
+};
